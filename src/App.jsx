e169521774c091e7d8493c1ddbbc502c0bd86e3f
Tomikimi <tomikimiogoli@yahoo.com>
2026-02-19 17:58:18 +0100

    Implementation of Components

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return <></>;
+}
+
+function Bill() {
+  return (
+    <>
+      <div>
+        <p>How Much</p>
+      </div>
+    </>
+  );
+}
+
+function Service() {
+  return <></>;
+}
+
+function BillServiceCalc() {
+  return <></>;
+}
+
+export default App;
